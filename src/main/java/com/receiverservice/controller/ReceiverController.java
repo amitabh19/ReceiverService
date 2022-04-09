378048b8b750fecf62886458267d35f1ff9b8b74
Amitabh Saxena <45675885+amitabh19@users.noreply.github.com>
2022-04-09 19:43:01 +0530

    added methods in service and controllers and poc

--- src/main/java/com/receiverservice/controller/ReceiverController.java
+++ src/main/java/com/receiverservice/controller/ReceiverController.java
@@ -1,10 +1,31 @@
 package com.receiverservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.receiverservice.entities.Transaction;
+import com.receiverservice.service.ReceiverService;
 
 @RestController
 @RequestMapping("/receiver")
 public class ReceiverController {
+	@Autowired
+	private ReceiverService receiverService;
 	
+	@PostMapping(path="/receiveTransaction")
+	public ResponseEntity<Transaction> decryptTransactionObject(@RequestBody Transaction t) {
+		return receiverService.receieveTransaction(t);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Transaction> getScreen(@PathVariable("id") String id){
+		return new ResponseEntity<Transaction>(receiverService.getTransByAccountNo(id),HttpStatus.OK);
+	}
 }
--- src/main/java/com/receiverservice/service/ReceiverService.java
+++ src/main/java/com/receiverservice/service/ReceiverService.java
@@ -3,7 +3,14 @@
  */
 package com.receiverservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.receiverservice.decryption.DecryptObject;
+import com.receiverservice.entities.Transaction;
+import com.receiverservice.repositories.TransactionRepositories;
 
 /**
  * @author amitabh
@@ -12,4 +19,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReceiverService {
 	
+	@Autowired
+	TransactionRepositories transactionRepository;
+
+	public ResponseEntity<Transaction> receieveTransaction(Transaction t) {
+		System.out.println("encrypted" + t.toString());
+		t.setAccountNumber(DecryptObject.decrypt(t.getAccountNumber()));
+		t.setAccountFrom(DecryptObject.decrypt(t.getAccountFrom()));
+		t.setAmount(DecryptObject.decrypt(t.getAmount()));
+		t.setCurrency(DecryptObject.decrypt(t.getCurrency()));
+		t.setType(DecryptObject.decrypt(t.getType()));
+		System.out.println("decrypted" + t.toString());
+		t = transactionRepository.save((t));
+		System.out.println("added to db");
+		return new ResponseEntity<Transaction>(t, new ResponseEntity<>(HttpStatus.ACCEPTED).getStatusCode());
+
+	}
+
+	public Transaction getTransByAccountNo(String id) {
+		return transactionRepository.findById(id).get();
+
+	}
 }
